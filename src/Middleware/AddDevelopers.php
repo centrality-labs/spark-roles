b0555b6100faa9b0df66d34b870ede6f6625ebef
Jon Nicholson <macro@drjonnicholson.com>
2017-01-26 23:27:56 +0000

    Changed composer constraints and added try/catch

--- src/Middleware/AddDevelopers.php
+++ src/Middleware/AddDevelopers.php
@@ -5,6 +5,8 @@ namespace ZiNETHQ\SparkRoles\Middleware;
 use Closure;
 use Laravel\Spark\Spark;
 use ZiNETHQ\SparkRoles\Models\Role;
+use Exception;
+use Log;
 
 class AddDevelopers extends AbstractMiddleware
 {
@@ -18,22 +20,26 @@ class AddDevelopers extends AbstractMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (config('sparkroles.developer.enable')) {
-            $role = Role::where('slug', config('sparkroles.developer.slug'))->first();
-            if ($role) {
-                $developers = [];
+        try {
+            if (config('sparkroles.developer.enable')) {
+                $role = Role::where('slug', config('sparkroles.developer.slug'))->first();
+                if ($role) {
+                    $developers = [];
 
-                foreach ($role->users as $user) {
-                    $developers[] = $user->email;
-                }
-
-                foreach ($role->teams as $team) {
-                    foreach ($team->users as $user) {
+                    foreach ($role->users as $user) {
                         $developers[] = $user->email;
                     }
+
+                    foreach ($role->teams as $team) {
+                        foreach ($team->users as $user) {
+                            $developers[] = $user->email;
+                        }
+                    }
+                    Spark::developers(array_merge(Spark::$developers, $developers));
                 }
-                Spark::developers(array_merge(Spark::$developers, $developers));
             }
+        } catch (Exception $e) {
+            Log::error("Error: {$e->getMessage()}\n{$e->getTraceAsString()}");
         }
         return $next($request);
     }
